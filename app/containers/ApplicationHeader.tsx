5afa95c2e49e8189dd36a0802ae71addd44330ca
Donte Littlejohn <donteravae@gmail.com>
2024-05-06 14:08:57 -0400

    wrapped logo with Link component in application header

--- app/containers/ApplicationHeader.tsx
+++ app/containers/ApplicationHeader.tsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 import { loader } from "../root";
 import UserAuthAccess from "./UserAuthAcess";
 import AccountDropdown from "./AccountDropdown";
@@ -6,12 +6,14 @@ import AccountDropdown from "./AccountDropdown";
 export default function ApplicationHeader() {
   const { profile } = useLoaderData<typeof loader>();
   return (
-    <header className="grid grid-cols-[30%_1fr_15%] grid-rows-1 items-center col-span-full border-b px-5">
-      <img
-        className="h-full"
-        src="assets/logo_trans_horizontal.png"
-        alt="SPADE Mental Health Logo"
-      />
+    <header className="grid grid-cols-[auto_1fr_15%] grid-rows-1 items-center col-span-full border-b px-5">
+      <Link to="/" className="h-full">
+        <img
+          className="h-full"
+          src="assets/logo_trans_horizontal.png"
+          alt="SPADE Mental Health Logo"
+        />
+      </Link>
       <form>
         <input type="search" />
       </form>
