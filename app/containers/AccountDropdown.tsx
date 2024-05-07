cd9e237df5c2f04fc8ec63b6fb571037ad75c783
Donte Littlejohn <donteravae@gmail.com>
2024-05-07 16:35:49 -0400

    connected logout menu item to root action; added menu items to view user profile and access settings; added icons to prefix menu item label

--- app/containers/AccountDropdown.tsx
+++ app/containers/AccountDropdown.tsx
@@ -1,8 +1,15 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+
 import { Form, Link } from "@remix-run/react";
 import { UserProfile } from "../lib/types";
 import UserAvatar from "../components/UserAvatar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowRightToBracket,
+  faCaretDown,
+  faGear,
+} from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 
 export default function AccountDropdown({ profile }: { profile: UserProfile }) {
@@ -12,9 +19,9 @@ export default function AccountDropdown({ profile }: { profile: UserProfile }) {
   const handleClick = () => setShowMenu((prev) => !prev);
 
   return (
-    <div className={`h-full relative flex p-1 ${showMenu ? "" : ""}`}>
+    <nav className={`h-full relative flex p-1`}>
       <button
-        className="h-full w-full flex justify-end items-center gap-2 border-none bg-none cursor-pointer"
+        className="h-full w-full flex justify-end items-center gap-2 py-2 border-none bg-none cursor-pointer"
         onClick={handleClick}
       >
         <h4 className="m-0">{username}</h4>
@@ -23,26 +30,53 @@ export default function AccountDropdown({ profile }: { profile: UserProfile }) {
       </button>
       <menu
         className={`w-full block m-0 absolute top-[60px] p-0 shadow-2xl rounded-md ${
-          showMenu ? "bg-black z-10 right-0" : "right-[9999px]"
+          showMenu ? "bg-black z-20 right-0" : "right-[9999px]"
         }`}
       >
-        <li className="flex w-full items-center h-10">
+        <li className="flex w-full items-center h-12">
           <Link
-            className="flex-auto py-2 px-3 text-white hover:bg-white hover:text-black text-left"
+            className="h-full flex items-center flex-auto gap-2 p-3 text-white hover:bg-white hover:text-black text-left"
+            onClick={handleClick}
+            onBlur={handleClick}
             to={`/users/${username}`}
-            reloadDocument
           >
+            <UserAvatar username={username} avatarUrl={avatar} />
             My Profile
           </Link>
         </li>
-        <li className="flex h-10">
+        <li className="flex w-full items-center h-12">
+          <Link
+            className="h-full flex items-center flex-auto gap-2 p-3 text-white hover:bg-white hover:text-black text-left"
+            onClick={handleClick}
+            to={`/settings`}
+          >
+            <FontAwesomeIcon icon={faGear} className="h-4 w-6" />
+            Settings
+          </Link>
+        </li>
+        <li className="flex h-12">
           <Form method="post" className="flex-auto">
-            <button className="w-full py-2 px-3 text-white hover:bg-white hover:text-black text-left">
+            <button
+              className="w-full flex items-center gap-2 p-3 text-white hover:bg-white hover:text-black text-left"
+              onClick={handleClick}
+              name="requestType"
+              value="logout-request"
+            >
+              <FontAwesomeIcon
+                icon={faArrowRightToBracket}
+                className="h-4 w-6"
+              />
               Log Out
             </button>
           </Form>
         </li>
       </menu>
-    </div>
+      <div
+        className={
+          showMenu ? "block fixed top-0 right-0 left-0 bottom-0 z-10" : "hidden"
+        }
+        onClick={handleClick}
+      />
+    </nav>
   );
 }
--- app/containers/ApplicationHeader.tsx
+++ app/containers/ApplicationHeader.tsx
@@ -5,9 +5,10 @@ import AccountDropdown from "./AccountDropdown";
 
 export default function ApplicationHeader() {
   const { profile } = useLoaderData<typeof loader>();
+
   return (
-    <header className="grid grid-cols-[auto_1fr_15%] grid-rows-1 items-center col-span-full border-b px-5">
-      <Link to="/" className="h-full">
+    <header className="grid grid-cols-[135px_1fr_15%] grid-rows-1 items-center col-span-full border-b px-5">
+      <Link to="/" className="h-full" reloadDocument>
         <img
           className="h-full"
           src="/assets/logo_trans_horizontal.png"
