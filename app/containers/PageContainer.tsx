e66d6eb552f37501d6f4eae8b9bf65765929dac4
Donte Littlejohn <donteravae@gmail.com>
2024-05-10 14:33:54 -0400

    added the option for custom class names

--- app/containers/PageContainer.tsx
+++ app/containers/PageContainer.tsx
@@ -1,5 +1,11 @@
 import { ReactNode } from "react";
 
-export default function PageContainer({ children }: { children: ReactNode }) {
-  return <main className="flex flex-wrap px-28 py-5 gap-3">{children}</main>;
+export default function PageContainer({
+  className,
+  children,
+}: {
+  className?: string;
+  children: ReactNode;
+}) {
+  return <main className={`px-16 py-5 gap-3 ${className}`}>{children}</main>;
 }
